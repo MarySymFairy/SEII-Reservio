5b6f8555a35c05e048ccd1c0593ff7370f6928d2
Maria Mamougiorgi <93185018+MarySymFairy@users.noreply.github.com>
2024-12-08 16:31:54 +0200

    Revert "changes"
    This reverts commit a48d9092936e28e6de9bcc2c1d4048498930801f.

--- tests/businessReservations.test.js
+++ tests/businessReservations.test.js
@@ -1,20 +1,22 @@
 const http = require('http');
 const test = require('ava');
 const got = require('got');
+const listen = require('test-listen');
 const app = require('../index.js');
 
 test.before(async (t) => {
-  t.context.server = http.createServer(app);
-  const server = t.context.server.listen();
-  const { port } = server.address();
-  t.context.got = got.extend({
-    responseType: 'json',
-    prefixUrl: `http://localhost:${port}`,
-  });
+    t.context.server = http.createServer(app);
+    t.context.prefixUrl = await listen(t.context.server);
+    t.context.got = got.extend({
+        http2: true,
+        throwHttpErrors: false,
+        responseType: "json",
+        prefixUrl: t.context.prefixUrl,
+    });
 });
 
 test.after.always((t) => {
-  t.context.server.close();
+    t.context.server.close();
 });
 
 // GET /business-reservations - Retrieve all reservations
--- tests/businesses.test.js
+++ tests/businesses.test.js
@@ -1,20 +1,19 @@
-const http = require('http');
-const test = require('ava');
-const got = require('got');
+const http = require("node:http");
+
+const test = require("ava");
+const got = require("got");
+
 const app = require('../index.js');
 
-test.before(async (t) => {
-  t.context.server = http.createServer(app);
-  const server = t.context.server.listen();
-  const { port } = server.address();
-  t.context.got = got.extend({
-    responseType: 'json',
-    prefixUrl: `http://localhost:${port}`,
-  });
+test.before(async t => {
+    t.context.server = http.createServer(app);
+    const server = t.context.server.listen();
+    const { port } = server.address();
+    t.context.got = got.extend({ responseType: 'json', prefixUrl: `http://localhost:${port}` });
 });
 
-test.after.always((t) => {
-  t.context.server.close();
+test.after.always(t => {
+    t.context.server.close();
 });
 
 
