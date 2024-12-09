c1f5e01459f84c2f0539026e9c393d883ac0e180
Eirini Smyrnaki <eirinismyrnaki@gmail.com>
2024-12-10 01:26:54 +0200

    Update init.test.js

--- tests/init.test.js
+++ tests/init.test.js
@@ -19,7 +19,7 @@ test.after.always(t => {
 ////GET /businesses //////////////////////////////////////////////////////////////////////////////////////////
 //Happy path: Retrieve all businesses
 test("GET /user/{userID}/businesses - Retrieve all businesses", async t => {
-    const { body, statusCode } = await t.context.got.get('user/1/businesses?category-name=Breakfast');
+    const { body, statusCode } = await t.context.got.get('users/1/businesses?category-name=Breakfast');
     t.is(statusCode, 200);
     t.true(Array.isArray(body));
     t.true(body.length > 0);
@@ -27,29 +27,29 @@ test("GET /user/{userID}/businesses - Retrieve all businesses", async t => {
 )
 //Error case: Get businesses by invalid category
 test("GET /user/{userID}/businesses - Get businesses by invalid category", async t => {
-    const { body, statusCode } = await t.context.got("user/1/businesses?category-name=InvalidCategory", {
+    const { body, statusCode } = await t.context.got("users/1/businesses?category-name=InvalidCategory", {
         throwHttpErrors: false,
     });   
     t.is(statusCode, 404);
 });
 //Happy path: Search business by keyword
 test("GET /user/{userID}/businesses/search - Search business by keyword", async t => {
-    const { body, statusCode } = await t.context.got("user/1/businesses/search?keyword='keyword'");
+    const { body, statusCode } = await t.context.got("users/1/businesses/search?keyword='keyword'");
     t.is(statusCode, 200);
     t.true(Array.isArray(body));
     t.is(body[0].keyword, "keyword");
 }
 )
 //Error case: Search business by invalid keyword
-test("GET /user/{userID}/businesses/search - Search business by invalid keyword", async t => {
-    const {body, statusCode} = await t.context.got("user/1/businesses/search?keyword=InvalidKeyword", {
+test("GET /users/{userID}/businesses/search - Search business by invalid keyword", async t => {
+    const {body, statusCode} = await t.context.got("users/1/businesses/search?keyword=InvalidKeyword", {
         throwHttpErrors: false
     });
     t.is(statusCode, 404);
 });
 //Error case: Get businesses by invalid user ID
 test("GET /user/{userID}/businesses - Get businesses by invalid user ID", async t => {
-    const {body, statusCode} = await t.context.got("user/999/businesses?category-name=breakfast", {
+    const {body, statusCode} = await t.context.got("users/999/businesses?category-name=breakfast", {
         throwHttpErrors: false
     });
     t.is(statusCode, 404);
