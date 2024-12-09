602fcf29e91f5b1e9bdd42fca05da1a5ba6383a3
Maria Mamougiorgi <93185018+MarySymFairy@users.noreply.github.com>
2024-12-09 19:58:02 +0200

    small change

--- tests/checkSomething.test.js
+++ tests/checkSomething.test.js
@@ -1,4 +1,23 @@
-const test = require('ava');
+const http = require("http");
+const test = require("ava");
+const got = require("got");
+
+const app = require("../index.js");
+
+test.before(async (t) => {
+  t.context.server = http.createServer(app);
+  const server = t.context.server.listen();
+  const { port } = server.address();
+  t.context.got = got.extend({
+    responseType: "json",
+    prefixUrl: `http://localhost:${port}`,
+  });
+});
+
+test.after.always((t) => {
+  t.context.server.close();
+});
+
 const { getBusinessesByCategory } = require('../services/DefaultService');
 test('getBusinessesByCategory returns businesses for a valid category', async t => {
   const categoryName = 'Breakfast';
