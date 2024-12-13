b2d85cb58b03aec386b935c76941e10e5b9281cc
Maria Mamougiorgi <93185018+MarySymFairy@users.noreply.github.com>
2024-12-13 13:44:36 +0200

    changes

--- tests/businessReservations.test.js
+++ tests/businessReservations.test.js
@@ -19,19 +19,41 @@
 //     t.context.server.close();
 // });
 
-// // GET /business-reservations - Retrieve all reservations
+// GET /businessReservations
 
-// //Happy path: Retrieve all reservations 
-// test("GET /business-reservations - Retrieve all reservations (happy path)", async (t) => {
-//     const { body, statusCode } = await t.context.got.get("business-reservations");
-//     t.is(statusCode, 200);
-//     t.true(Array.isArray(body));
-// });
+//Happy path: Retrieve all reservations 
+test("GET /businessReservations - Retrieve all reservations", async (t) => {
+    const { body, statusCode } = await t.context.got.get("businessReservations"); //or business - reservations
+    t.is(statusCode, 200);
+    t.true(Array.isArray(body));
+});
 
 
-// //Unhappy path: Get businesses reservations with invalid query parameter
-// test("GET /businessesReservations - Invalid query parameters", async (t) => {
-//     const error = await t.throwsAsync(() => t.context.got("businessesReservations?invalidParam=value"));
-//     t.is(error.response.statusCode, 400);
-//     t.is(error.response.body.message, "Invalid query parameter");
-// });
+//Unhappy path: Get businesses reservations with invalid query parameter
+test("GET /businessReservations - Invalid query parameters", async (t) => {
+    const error = await t.throwsAsync(() => t.context.got.get("businessesReservations?invalidParam=value"));
+    t.is(error.response.statusCode, 400);
+    t.is(error.response.body.message, "Invalid query parameter");
+});
+
+//Unhappy path: No existing reservations
+ test("GET /businessReservations - No existing reservations/", async (t) => {
+    const error = await t.throwsAsync(() => t.context.got.get("businessReservations"));
+    t.is(error.response.statusCode, 404);
+    t.is(error.response.body.message, "No business reservations found.");
+  }); 
+  
+
+//Unhappy path: Nonexistent Resource
+test("GET /businessReservations/:id - Nonexistent businessReservations", async (t) => {
+    const error = await t.throwsAsync(() => t.context.got.get("businessReservations/99999"));
+    t.is(error.response.statusCode, 404);
+    t.is(error.response.body.message, "Business reservations not found");
+});
+
+// Unhappy path: Invalid ID format when fetching a business reservation
+test("GET /businessReservations/:id - Invalid ID format", async (t) => {
+    const error = await t.throwsAsync(() => t.context.got.get("businessReservations/invalid-id"));
+    t.is(error.response.statusCode, 400);
+    t.is(error.response.body.message, "Invalid reservation ID format");
+});
