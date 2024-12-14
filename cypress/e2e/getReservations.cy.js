534c647d8f2f05b8fedb76107cd15999731e91d6
Maria Mamougiorgi <93185018+MarySymFairy@users.noreply.github.com>
2024-12-14 22:23:07 +0200

    frontend RIGHT (for now)

--- cypress/e2e/getReservations.cy.js
+++ cypress/e2e/getReservations.cy.js
@@ -13,19 +13,21 @@ describe('TEST endpoint GET reservations & GET reservations/id', () => {
     cy.get('#operations-reservation-viewAReservation').click();
     cy.contains('Parameters').should('be.visible'); // Check if the parameters section is shown
     cy.get('.btn.try-out__btn').should('be.visible').click(); // Click on the "Try it out" button
-    cy.get('input[name="reservationId"]').type('12345'); // Enter an id
-    cy.get('input[name="userId"]').type('6');
+    cy.get('input[placeholder="reservationId - ReservationId of the reservation submitted to the system"]').type('12345'); // Enter an id
+    cy.get('input[placeholder="userId - UserId of the logged in user that made the reservation"]').type('6');
     cy.get('.btn.execute.opblock-control__btn').click(); // Click on the "Execute" button
     cy.contains('Reservation not found').should('be.visible'); // Check if the response is shown
   });
 
   it('should display a list of reservations', () => {
-    cy.visit('http://localhost:8080/reservations');
     cy.get('#operations-reservation-viewReservations').click();
     cy.contains('Parameters').should('be.visible'); // Check if the parameters section is shown
     cy.get('.btn.try-out__btn').should('be.visible').click(); // Click on the "Try it out" button
-    cy.get('.reservation-list').should('exist');
-    cy.get('.reservation-item').should('have.length.greaterThanOrEqual', 0);
+    cy.get('input[placeholder="userId - UserId of the logged in user that made the reservation"]').type('6');
+    cy.get('.btn.execute.opblock-control__btn').click(); // Click on the "Execute" button
+    cy.contains('Response body').should('be.visible'); // Ensure response section is visible
+    cy.get('.microlight').should('contain.text', '"reservationId"'); // Check JSON contains reservation data  
+      
   });
-  
+
 });
